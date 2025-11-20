27deb1a2acb3b8642b989b3b1a8c9a86e492fb9b
Tea Alushi <tea.alushi@gmail.com>
2025-11-20 21:40:43 +0100

    cicd

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,44 +3,45 @@ import { defineConfig, devices } from "@playwright/test";
 
 dotenvConfig();
 
+const isCI = !!process.env.CI;
+
 const crossBrowserConfig = {
   testDir: "./tests/visual/cross-browser",
-  snapshotPathTemplate: ".test/cross/{testFilePath}/{arg}{ext}",
+  snapshotPathTemplate: ".test/snaps/{projectName}/{testFilePath}/{arg}{ext}",
   expect: {
     toHaveScreenshot: { maxDiffPixelRatio: 0.1 },
   },
 };
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
+  timeout: 1000 * 60,
+  workers: isCI ? 1 : undefined,
+  retries: isCI ? 2 : 0,
+  forbidOnly: isCI,
   testDir: "./tests",
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-    baseURL: process.env.E2E_BASE_URL,
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    // trace: 'on-first-retry',
+  outputDir: ".test/spec/output",
+  snapshotPathTemplate:
+    ".test/spec/snaps/{projectName}/{testFilePath}/{arg}{ext}",
+  testMatch: "*.spec.{ts,tsx}",
+  reporter: [
+    [
+      "html",
+      {
+        outputFolder: ".test/spec/results",
+        open: "never",
+      },
+    ],
+    isCI ? ["github"] : ["line"],
+  ],
+  use: {
+    baseURL: process.env.E2E_BASE_URL,
     trace: "retain-on-failure",
     screenshot: "only-on-failure",
     video: "retain-on-failure",
     headless: process.env.CI === "true" ? true : false,
   },
-
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "cross-chromium",
@@ -61,32 +62,5 @@ export default defineConfig({
     //   dependencies: ["cross-firefox"],
     //   ...crossBrowserConfig,
     // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
---
+++ tests/visual/cross-browser/landingpage.spec.ts
@@ -0,0 +1,18 @@
+import test, { expect } from "@playwright/test";
+
+test("cross-browser snapshots", async ({ page }) => {
+  test.setTimeout(60000);
+  await page.goto("/");
+  await page.locator("video").first().waitFor({ state: "visible" });
+  await expect(page.locator("video")).not.toHaveCount(0);
+  const allVideos = await page.locator("video").all();
+  for (const video of allVideos) {
+    await expect(async () => {
+      await expect(video).toHaveAttribute("src");
+    }).toPass({ timeout: 30000, intervals: [500] });
+  }
+  await expect(page).toHaveScreenshot(`home-page.png`, {
+    fullPage: true,
+    // animations: "disabled",
+  });
+});
